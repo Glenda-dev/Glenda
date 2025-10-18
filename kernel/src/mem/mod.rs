bbe4d3d59e505821c7aa8fe6614c6dbb4dc73e92
mtx <mitchell.xu2@gmail.com>
2025-10-18 22:33:47 +0800

    [mem] Increase KERN_PAGES for hardware boot

--- kernel/src/mem/mod.rs
+++ kernel/src/mem/mod.rs
@@ -4,7 +4,7 @@ pub const PGSIZE: usize = 4096;
 pub const PGNUM: usize = PGSIZE / core::mem::size_of::<usize>(); // 2^9
 pub const PGMASK: usize = PGSIZE - 1;
 pub const VA_MAX: usize = 1 << 38;
-pub const KERN_PAGES: usize = 1024;
+pub const KERN_PAGES: usize = 8192;
 
 pub mod addr;
 pub mod pmem;
