00a63175dfe74ed7f0ed0c9829ba13b598629c78
2018wzh <wzh20070720@outlook.com>
2025-10-25 23:14:52 +0800

    [kernel][test] Update syscall test

--- kernel/src/tests/syscall.rs
+++ kernel/src/tests/syscall.rs
@@ -1,6 +1,6 @@
 use crate::printk;
 use crate::printk::{ANSI_GREEN, ANSI_RESET, ANSI_YELLOW};
-use crate::proc::launch;
+use crate::proc::process::launch_payload;
 
 // 由 build.rs 生成，这个文件可能为空
 // FIXME: 由于 xtask 构建流程的问题，暂时无法使用 OUT_DIR
@@ -24,9 +24,9 @@ pub fn run(hartid: usize) {
 fn launch_test_proc() -> ! {
     if HAS_PROC_PAYLOAD && !PROC_PAYLOAD.is_empty() {
         printk!("Launching external test payload");
-        launch(&PROC_PAYLOAD);
+        launch_payload(&PROC_PAYLOAD);
     } else {
         printk!("Launching internal test payload");
-        launch(&USER_INIT_CODE);
+        launch_payload(&USER_INIT_CODE);
     }
 }
