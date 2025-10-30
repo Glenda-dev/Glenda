b6143a3e8ff6bb8599e633ecf17a5476bb6a1c87
2018wzh <wzh20070720@outlook.com>
2025-10-30 14:15:15 +0800

    [kernel][refactor] Move assembly to asm folder

--- kernel/build.rs
+++ kernel/build.rs
@@ -1,16 +1,16 @@
 fn main() {
-    println!("cargo:rerun-if-changed=src/boot.S");
-    println!("cargo:rerun-if-changed=src/trap/vector.S");
-    println!("cargo:rerun-if-changed=src/init/sbi.S");
-    println!("cargo:rerun-if-changed=src/proc/enter.S");
-    println!("cargo:rerun-if-changed=src/proc/switch.S");
+    println!("cargo:rerun-if-changed=src/asm/boot.S");
+    println!("cargo:rerun-if-changed=src/asm/vector.S");
+    println!("cargo:rerun-if-changed=src/asm/sbi.S");
+    println!("cargo:rerun-if-changed=src/asm/enter.S");
+    println!("cargo:rerun-if-changed=src/asm/switch.S");
     println!("cargo:rerun-if-changed=src/linker.ld");
     cc::Build::new()
-        .file("src/boot.S")
-        .file("src/trap/vector.S")
-        .file("src/init/sbi.S")
-        .file("src/proc/enter.S")
-        .file("src/proc/switch.S")
+        .file("src/asm/boot.S")
+        .file("src/asm/vector.S")
+        .file("src/asm/sbi.S")
+        .file("src/asm/enter.S")
+        .file("src/asm/switch.S")
         .flag("-march=rv64gc")
         .flag("-mabi=lp64d")
         .compile("boot");
