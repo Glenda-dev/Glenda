49fae59b05fe93bab5927f1f68588027ac125771
mtx <mitchell.xu2@gmail.com>
2026-01-01 22:49:06 +0800

    [kernel][init] Remove unnecessary files

--- kernel/src/init/pmem.rs
+++
@@ -1,7 +0,0 @@
-use crate::mem::pmem::initialize_regions;
-use spin::Once;
-
-static PMEM_INIT: Once<()> = Once::new();
-pub fn init(hartid: usize, _dtb: *const u8) {
-    PMEM_INIT.call_once(|| initialize_regions(hartid));
-}
