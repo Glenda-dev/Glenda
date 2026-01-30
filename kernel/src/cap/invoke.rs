195059837eaca9a05935e933086d794e9d4ef0a8
2018wzh <wzh20070720@outlook.com>
2026-01-30 21:06:45 +0800

    [kernel][cap] Add permission check for console

--- kernel/src/cap/invoke.rs
+++ kernel/src/cap/invoke.rs
@@ -468,7 +468,7 @@ fn invoke_irq_handler(cap: &mut Capability, method: usize) -> usize {
     }
 }
 
-fn invoke_console(_cap: &mut Capability, method: usize) -> usize {
+fn invoke_console(cap: &mut Capability, method: usize) -> usize {
     let tcb = unsafe { &mut *scheduler::current().expect("No current TCB") };
     let utcb = match tcb.get_utcb() {
         Some(u) => u,
@@ -477,11 +477,17 @@ fn invoke_console(_cap: &mut Capability, method: usize) -> usize {
 
     match method {
         consolemethod::PUT_CHAR => {
+            if cap.has_rights(Rights::WRITE) == false {
+                return errcode::PERMISSION_DENIED;
+            }
             let c = utcb.mrs_regs[0] as u8 as char;
             crate::printk!("{}", c);
             errcode::SUCCESS
         }
         consolemethod::PUT_STR => {
+            if cap.has_rights(Rights::WRITE) == false {
+                return errcode::PERMISSION_DENIED;
+            }
             let offset = utcb.mrs_regs[0];
             let len = utcb.mrs_regs[1];
             if let Some(_) = utcb.with_str(offset, len, |s| {
@@ -493,6 +499,9 @@ fn invoke_console(_cap: &mut Capability, method: usize) -> usize {
             }
         }
         consolemethod::GET_CHAR => {
+            if cap.has_rights(Rights::READ) == false {
+                return errcode::PERMISSION_DENIED;
+            }
             let c = hal::console::read() as usize;
             utcb.mrs_regs[0] = c;
             errcode::SUCCESS
