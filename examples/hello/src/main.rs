1281bebf810ffdc0617e93959f320e28226fb0cf
2018wzh <wzh20070720@outlook.com>
2026-02-03 22:37:09 +0800

    [hello] Removed console

--- examples/hello/src/main.rs
+++ examples/hello/src/main.rs
@@ -2,11 +2,9 @@
 #![no_main]
 use glenda;
 use glenda::println;
-use glenda::runtime::KERNEL_CAP;
 
 #[unsafe(no_mangle)]
 fn main() -> usize {
-    KERNEL_CAP.shell();
     println!("Hello World!");
     0
 }
